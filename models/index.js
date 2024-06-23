c0890ad62693131d6e74fe30fccd8b4066fb6dd9
Flavio Repos <buzonplataforma12@gmail.com>
2024-06-23 11:45:35 -0600

    routes get and post ready

--- models/index.js
+++ models/index.js
@@ -5,12 +5,34 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreingKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreingKey: 'product_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique:false
+  },
+  as:'tagsofaProduct'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique:false
+  },
+  as:'sameTagProducts'
+});
+
 
 module.exports = {
   Product,
@@ -18,3 +40,4 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
